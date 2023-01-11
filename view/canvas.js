de07e89ea171bf6336407b59bf8ca633ac6e9ea0
IUT-Toulon-Maxence-Jamme <78029916+IUT-Toulon-Maxence-Jamme@users.noreply.github.com>
2023-01-11 22:05:53 +0100

    avance

--- view/canvas.js
+++ view/canvas.js
@@ -1,25 +1,52 @@
+
 window.onload = function() {
-	Draw_grid();
+	start();
 }
 
-function Draw_grid(){
-	console.log('okokok')
+function start(){
 	const canvas = new Canvas('dessin');
-	canvas.initCanvasGrid(24,10);
-
-
+	canvas.initCanvasGrid();
+	var Grille = [	[1,0,0,0,0,1,0,0,0,4],
+					[0,1,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,3,0,0],
+					[0,0,0,0,0,0,0,2,0,0],
+					[0,0,0,0,0,0,0,1,0,0],
+					[0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0],
+					[0,0,4,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,3,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,5,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0],
+					[3,5,1,2,3,4,4,4,5,5]]
+	canvas.drawGrid(Grille);
 }
-//const app = new Controller(new Model(), new View('dessins'));
-
 
 class Canvas {
 	constructor(div_id) {
 		this.div_id = div_id;
 		this.p_tag;
 		this.ctx = document.getElementById(this.div_id).getContext('2d');
+		this.height = 24;
+		this.width = 10;
+		this.size_case = 30;
+		this.pixel_height = this.height * this.size_case; 
+		this.pixel_width = this.width * this.size_case;
+		this.palette = ['#0ad6ff','#4efd54', "#bc13fe", "#cfff04", "#fe019a", "#ff073a"];
 	}
 
-	// Binding.
+	/*// Binding.
 	bindGetCNF (callback) {
 		this.getCNF = callback; // On veut pouvoir demander au Model (depuis le Controller) une nouvelle Chuck Norris Fact.
 	}
@@ -41,26 +68,75 @@ class Canvas {
 		if (this.p_tag) {
 			this.p_tag.innerHTML = cnf_value;
 		}
-	}
+	}*/
 
-	initCanvasGrid(height,width){
+	initCanvasGrid(){
+		this.ctx.fillStyle = "#121212";
+		this.ctx.fillRect(0, 0, this.size_case * this.width, this.size_case * this.height);
 		this.ctx.strokeStyle='white';
 		this.ctx.lineWidth=0.2;
-		var size_case = 30; //px
-		var pixel_height = height * size_case; 
-		var pixel_width = width * size_case;
-		console.log(pixel_height,pixel_width)
-		this.ctx.beginPath(); // Start a new path
-		for (var col=0; col < height; col++) {
-			this.ctx.moveTo(col*size_case,0); // Move the pen to (30, 50)
-			this.ctx.lineTo(col*size_case, pixel_height); // Draw a line to (150, 100)
+		console.log(this.pixel_height,this.pixel_width)
+		this.ctx.beginPath(); // Start
+		for (var col=0; col < this.width; col++) {
+			this.ctx.moveTo(col*this.size_case,0);
+			this.ctx.lineTo(col*this.size_case, this.pixel_height); // Draw a line 
 		}
-		for (var line=0; line <= height; line++) {
-			this.ctx.moveTo(0, line*size_case); // Move the pen to (30, 50)
-			this.ctx.lineTo(pixel_width, line*size_case); // Draw a line to (150, 100)
+		for (var line=0; line <= this.height; line++) {
+			this.ctx.moveTo(0, line*this.size_case);
+			this.ctx.lineTo(this.pixel_width, line*this.size_case); // Draw a line 
 		}
-		this.ctx.stroke(); // Render the path	
+		this.ctx.stroke(); // End	
 	}
 
-	draw_
+	drawGrid(grid){
+		for (var i = 0; i < this.height; i++) {
+			for (var j = 0; j < this.width; j++) {
+				if(grid[i][j]!=0){
+					this.ctx.fillStyle = this.palette[grid[i][j]];
+					this.ctx.fillRect(j*this.size_case + 1, i*this.size_case + 1, this.size_case - 2, this.size_case - 2);
+				}
+			}
+		}
+	}
+
+	async valideLine(line){
+		for (var i = 0; i <4; i++) {
+			for (var j = 0; j < this.width; j++) {
+				if (i % 2 == 0){
+					this.ctx.fillStyle = "white";
+					this.ctx.fillRect(j*this.size_case + 1, line*this.size_case + 1, this.size_case - 2, this.size_case - 2);
+				}else{
+					this.ctx.fillStyle = "#121212";
+					this.ctx.fillRect(j*this.size_case + 1, line*this.size_case + 1, this.size_case - 2, this.size_case - 2);
+				}
+			}
+			await sleep(100);
+		}
+	}
+}
+
+document.addEventListener('keyup', (event) => {
+	const nomTouche = event.key;
+	switch (event.key) {
+		case "ArrowDown":
+			console.log("ArrowDown");
+		break;
+		case "ArrowUp":
+			console.log("ArrowUp");
+		break;
+		case "ArrowLeft":
+			console.log("ArrowLeft");
+		break;
+		case "ArrowRight":
+			console.log("ArrowRight");
+		break;
+		default:
+			canvas.valideLine(23);
+		return; 
+	}
+}, false);
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
